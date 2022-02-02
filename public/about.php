104cc6619b70ac992e37a77fa2bf34d1ba7202fa
Kris Rana <36148361+krisrana@users.noreply.github.com>
2022-02-01 23:18:20 -0500

    Update about.php

--- public/about.php
+++ public/about.php
@@ -13,9 +13,9 @@
         <div class="container">    
             <div class="row">
                 <div class="col-sm col-lg col-md text-center text-justify" style="font-family:Palatino Linotype; font-size: 20px;">
-                Welcome to Capsule Corp! Here, we pride ourselves in cutting out the middleman, bringing your health directly to you! 
-                With our team of 8 specialists, we have designed this straightforward website using software such as HTML, PHP, SQL, XAMPP, Bootstrap, and fontAwesome. 
-                We know what it's like dealing with the middle man for your supply of health needs for you and your family. 
+                Welcome to Capsule Corp! Here, we pride ourselves in cutting out the middleman, bringing your health directly to you!<br> 
+                With our team of specialists, we have designed this straightforward website using software such as HTML, PHP, SQL, Bootstrap and fontAwesome library. This project also utilizes Paypal API to facilitate a payment system to complete the e-commerce experience.<b>(Fake Transaction)</b> <br> 
+                We know what it's like dealing with the middle man for your supply of health needs for you and your family. <br>
                 We strive to make the stressful process easier for all. With full transparency such as easy to read product information, 
                 and legitimate prices, we only sell products that we feel are the best for your needs, 
                 and have been approved by top of the line pharmaceuticals. Happy shopping!
@@ -27,28 +27,28 @@
                 <div class="row" style="font-family:Courier New;">
                     <div class="col-sm-4 col-lg-4 col-md-4 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Viren Desai</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Database Administrator 
                         </p>
                     </div>
                     <div class="col-sm-4 col-lg-4 col-md-4 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Timothy Ference</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Full-stack Developer
                         </p>
                     </div>
                     <div class="col-sm-4 col-lg-4 col-md-4 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Max Marra</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Documentation Specialist 
                         </p>
                     </div>
                     <div class="col-sm-4 col-lg-4 col-md-4 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Kevin Onuszko</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Frontend Developer 
                         </p>
@@ -62,21 +62,21 @@
                     </div>
                     <div class="col-sm-4 col-lg-4 col-md-4 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Kevin Rollins</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Documentation Assistant 
                         </p>
                     </div>
                     <div class="col-sm-6 col-lg-6 col-md-6 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Stefan Szczachor</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Product / Marketing Manager 
                         </p>
                     </div>
                     <div class="col-sm-6 col-lg-6 col-md-6 text-center">
                         <img src="../resources/assests/person.png" alt="Person" width="140" height="140">
-                        <h2>Allen Yang</h2>
+                        <h2>Krishna Rana</h2>
                         <p>
                             Database Assistant 
                         </p>
